7d895bdda03ca884af719e10402294a1616ebf22
kristian-girsang <kristian.girsang@ui.ac.id>
2023-01-18 18:30:53 +0700

    Make more obstacles. Obstacles can have better spawn logic. Debuggin collision that not working when add new obstacles

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 def display_score():
     current_time = int(pygame.time.get_ticks() / 100) - start_time
@@ -9,10 +10,28 @@ def display_score():
     # print(current_time)
     return current_time
 
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            if obstacle_rect.bottom == 300: screen.blit(snail_surf,obstacle_rect)
+            else: screen.blit(fly_surf,obstacle_rect)
+        
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+        
+        return obstacle_list
+    else: return []
+
+def collisions(player, obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect): return False
+    return True
 
 pygame.init()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('Runner Game')
+pygame.display.set_caption('Pixel Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 game_active = False
@@ -25,8 +44,11 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 # score_surf = test_font.render('A Snail and A Player', False, (64,64,64))
 # score_rect = score_surf.get_rect(center = (400, 50))
 
+# Obstacles
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surf.get_rect(midbottom = (850, 300))
+fly_surf = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
+
+obstacle_rect_list = []
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
@@ -42,6 +64,10 @@ game_name_rect = game_name.get_rect(center = (400,80))
 
 game_message = test_font.render('Press Space to Run', False, (111,196,169))
 game_message_rect = game_message.get_rect(center = (400,340))
+
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer,1500)
 
 while True:
     for event in pygame.event.get():
@@ -59,8 +85,13 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE: 
                 game_active = True
-                snail_rect.left = 800
                 start_time = int(pygame.time.get_ticks() / 100)
+        
+        if event.type == obstacle_timer and game_active:
+            if randint(0,2):
+                obstacle_rect_list.append(snail_surf.get_rect(midbottom = (randint(900,1100), 300)))
+            else:
+                obstacle_rect_list.append(fly_surf.get_rect(midbottom = (randint(900,1100), 210)))
                 
     if game_active:
         screen.blit(sky_surface, (0,0))
@@ -70,9 +101,9 @@ while True:
         # screen.blit(score_surf, score_rect)
         score = display_score()
 
-        snail_rect.x -= 4.5
-        if snail_rect.right <= 0: snail_rect.left = 800
-        screen.blit(snail_surf, snail_rect)
+        # snail_rect.x -= 4.5
+        # if snail_rect.right <= 0: snail_rect.left = 800
+        # screen.blit(snail_surf, snail_rect)
 
         # player
         player_gravity += 1
@@ -81,20 +112,29 @@ while True:
             player_rect.bottom = 300
         screen.blit(player_surf,player_rect)
 
+        # Obstacle movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
         # collision
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+        game_active = collisions(player_rect,obstacle_rect_list)
+        
     else:
         screen.fill((94,129,162))
         screen.blit(player_stand, player_stand_rect)
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80,300)
+        player_gravity = 0
 
         score_message = test_font.render(f'Your Score: {score}', False, (111,196,169))
         score_message_rect = score_message.get_rect(center = (400,330))
+        restart_message = test_font.render('Press Space to Restart', False, (111,196,169))
+        restart_message_rect = restart_message.get_rect(center = (400, 360))
         screen.blit(game_name, game_name_rect)
         if score == 0:
             screen.blit(game_message, game_message_rect)
         else:
             screen.blit(score_message, score_message_rect)
+            screen.blit(restart_message, restart_message_rect)
         
     pygame.display.update()
     clock.tick(60)
